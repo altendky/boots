99bd9d9535476eaf42dbcb45bd0fbdccfaedbd57
Kyle Altendorf <sda@fstab.net>
2019-03-25 10:42:50 -0400

    lfds

--- src/boots/_core.py
+++ src/boots/_core.py
@@ -158,7 +158,10 @@ def post_file(data):
 
 
 def request_remote_lock_build(archive_url, username, personal_access_token):
-    parameters = {'BOOTS_ARCHIVE_URL': archive_url}
+    parameters = {
+        'BOOTS_ARCHIVE_URL': archive_url,
+        'BOOTS_ENVIRONMENTS': '|Linux-3.6-x64|macOS-3.6-x64',
+    }
 
     response = requests.post(
         url=(
